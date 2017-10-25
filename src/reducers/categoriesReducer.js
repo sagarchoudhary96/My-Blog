a1ac342d98b09b949639e8b862c1fe9dd2ce43ac
Sagar Choudhary <sagar.choudhary96@gmail.com>
2017-10-25 10:06:12 +0530

    Add: Categories Action and Reducers

---
+++ src/reducers/categoriesReducer.js
@@ -0,0 +1,27 @@
+export const categories = [
+      {
+        name: 'react',
+        path: 'react'
+      },
+      {
+        name: 'redux',
+        path: 'redux'
+      },
+      {
+        name: 'udacity',
+        path: 'udacity'
+      }
+  ]
+
+export const category = (state = {categories}, action) => {
+  switch (action.type) {
+    case 'GET_CATEGORIES_ACTION':
+      return {
+        ...state,
+        categories: action.categories
+      }
+
+    default:
+      return state
+  }
+}
