e14ddbe5f7032d149b600ac56e813f6054054bab
Sagar Choudhary <sagar.choudhary96@gmail.com>
2017-11-29 16:22:51 +0530

    update: cssstyles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import {BrowserRouter} from 'react-router-dom'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import App from './components/App'
+import reducer from './reducers'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+ReactDOM.render(<Provider store={store}>
+  <BrowserRouter><App/></BrowserRouter>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
